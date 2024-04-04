16252dd813dde188bf758853249fd996af0398c8
learnsom <166076409+learnsom@users.noreply.github.com>
2024-04-05 09:31:20 +1030

    Create config.js

---
+++ Emoji Dictionary/config.js
@@ -0,0 +1,7 @@
+{
+  "name": "Pythoners",
+  "website": "https://twitter.com/Pythoners",
+  "description": "Learn Python to build better applications",
+  "logo": "https://raw.githubusercontent.com/learnsom/Python/master/img/logo.png",
+  "twitter": "https://twitter.com/Pythoners"
+}
